93fe91c07b11ada819d0497d1137e9dba8b7ea3a
fred <fredryce@gmail.com>
2018-11-26 18:49:48 -0800

    updated

--- alex.py
+++ alex.py
@@ -8,40 +8,67 @@ import random
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
 from bs4 import BeautifulSoup as soup
+import datetime
 
-def runpowerreport():
+def generateReport():
 	value = subprocess.Popen(['powercfg', '/batteryreport' ,'/output', 'test.html'])
-	value.wait()
-	file = open('test.html', 'r')
+	return value
+def runpowerreport():
+	file = open('test.html', 'rb')
 	my_soup = soup(file, 'html.parser')
 	#result =  my_soup.findAll("tr", {"class": "odd dc 30"})[0]
 	result = my_soup.findAll('table')[2].findAll('tr')[-1].findAll('td')
 	time = result[0].findAll('span', {"class":"time"})[0].text
+	time = datetime.datetime.strptime(time,"%H:%M:%S")#.strftime('%H:%M:%S')
+
 	percent = int(result[-2].text.split()[0])
 	mwh = int(result[-1].text.split()[0].replace(',', ''))
 	print(time)
 	print(percent)
 	print(mwh)
 
+	return time, percent, mwh
+
 def process(p):
-	batt_start = psutil.sensors_battery().secsleft
-	for i in range(5):
-		#randomNum = random.randrange(5)
+	current_time = datetime.datetime.now().minute
+	while current_time < p[2].minute:
 		name = p[0].name()
 		cpu_percent = p[0].cpu_percent(interval=0.1)
 		battery = psutil.sensors_battery()
 		if float(cpu_percent) > 0 and name != "System Idle Process":
 			##do calculations here
+			if name in results:
+				results[name].append(cpu_percent)
+			else:
+				results[name] = list([cpu_percent])
 			b= "Process: {}\nCPU: {}% Memory: {:2f}% Battery Used: {:0.2f}%\n".format(name, cpu_percent, p[0].memory_percent(),(battery.secsleft/p[1]))
 			print(b)
-			#pass
-	#batt_diff = (batt_start - psutil.sensors_battery().secsleft) / p[1]
-	#print("process: {} took battery {}".format(p[0].name(), batt_diff))
+		current_time = datetime.datetime.now().minute
+def findmean(results, diff_mwh):
+	for key, value in results.items():
+		average = sum(value)/float(len(value))
+		results[key] = (average/diff_mwh)*100
+	return results
+
+
 
 
 if __name__ == "__main__":
+	results = {}
 	executor = ThreadPoolExecutor()
 	battery_life = psutil.sensors_battery()
-	runpowerreport()
-	#for i in psutil.process_iter():
-		#executor.submit(process, (i,battery_life.secsleft))
+	process_gen_report = generateReport()
+	process_gen_report.wait()
+	time, per, mwh = runpowerreport()
+	end_time = time + datetime.timedelta(minutes=4)
+	for i in psutil.process_iter():
+		future = executor.submit(process, (i,battery_life.secsleft, end_time))
+	executor.shutdown(True)
+	print(results)
+
+	process_gen_report = generateReport()
+	process_gen_report.wait()
+	time_new, per_new, mwh_new = runpowerreport()
+	diff_mwh = abs(mwh_new - mwh)
+	results = findmean(results, diff_mwh)
+	print(results)
